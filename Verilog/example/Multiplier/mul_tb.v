b4e52be76c2c9f04ab9f2a40505c2fc2d99d3575
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-07-11 11:59:54 +0600

    Update mul_tb.v

--- Verilog/example/Multiplier/mul_tb.v
+++ Verilog/example/Multiplier/mul_tb.v
@@ -1,15 +1,31 @@
-Test_bench:
-module mult32bit_tb;
-reg [31:0]a,b;
-regclk,rst;
-wire [63:0]cout;
-mult32bit stage1(clk,rst,a,b,cout);
-initial begin
-clk = 0; rst = 0; a=0;b=0;
-#20 rst =1 ;
-#3000 $finish;
-end
-always #5 clk=~clk;
-always #10 a=a+1;
-always #20 b=b+1;
+module tb_mulNbit;
+	parameter N = 32;
+	reg [N-1:0] a, b;
+	reg cin, clk, rst;
+	wire [N-1:0] sum;
+	wire cout;
+	
+	mulNbit inst (
+		.a(a),
+		.b(b),
+		.clk(clk),
+		.cin(cin),
+		.rst(rst),
+		.sum(sum),
+		.cout(cout)
+	);
+	
+	initial begin
+		$dumpfile("tb_mulNbit.vcd");
+		$dumpvars(0, tb_mulNbit);
+		
+		a <=0; b <=0; cin <= 0; clk <=0; rst <= 0;
+		
+		#1 rst = 1;
+		#500 $finish;
+	end
+	
+	always #2 clk = ~clk;
+	always #5 a = a + 1;
+	always #10 b = b + 1;
 endmodule
