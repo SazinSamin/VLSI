4fedc05d0b5ca49eaa40a4797727f23eeb547a73
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-07-11 11:43:49 +0600

    Update half_adder.v

--- Verilog/example/Half adder/half_adder.v
+++ Verilog/example/Half adder/half_adder.v
@@ -1,10 +1,11 @@
 module half_adder( input [2:0] a, 
 				input [2:0] b,
+				input clk,
 				output reg [4:0] sum,
 				output reg cout
 				);
 
-	always @ (*)
+	always @ (posedge clk)
 		{cout, sum} =  a + b;
 	
 endmodule
