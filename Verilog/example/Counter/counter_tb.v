5cffa6a19ea48d3cdbe58f018fc5d45be2b837aa
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-07-11 10:17:39 +0600

    Update counter_tb.v

--- Verilog/example/Counter/counter_tb.v
+++ Verilog/example/Counter/counter_tb.v
@@ -1,16 +1,32 @@
-module cntr8bit_tb;
-reg rst,clk;
-wire [7:0]count;
-cntr8bit stage1(count,rst,clk);
-initial begin
-clk=0;rst=0;
-#15 rst = 1;
-#3000 rst=0;
-
-
-#10 $finish;
-end
-always #5 clk=~clk;
-//always #10 rst=~rst;
-
+module tb_counter;
+	reg clk;
+	reg rst;
+	wire [3:0] out;
+	
+	counter c0 (
+		.clk(clk),
+		.rst(rst),
+		.out(out)
+	);
+		
+	initial begin 
+	$dumpfile("tb_counter.vcd");
+	$dumpvars(0, tb_counter);
+    
+		clk <= 0;
+		rst <= 0;
+		#15 rst = 1;
+		#500 $finish;
+	end
+	
+	always #5 clk = ~clk;
+	
 endmodule
+		
+		
+		
+		
+		
+		
+		
+		
