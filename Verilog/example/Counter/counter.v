9fecbd5558f585cde966d4864d204ebb16152457
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-07-11 10:17:13 +0600

    Update counter.v

--- Verilog/example/Counter/counter.v
+++ Verilog/example/Counter/counter.v
@@ -1,10 +1,12 @@
-module cntr8bit(count,rst,clk);
-input rst,clk;
-output reg [7:0]count;
-always  @(posedge clk)  begin
-if(~rst)
-count<=0;
-else
-count<=count+1;
-end
+module counter( input clk, 
+			 input rst, 
+			 output reg [3:0] out);
+		
+	always @ (posedge clk) 
+		begin
+			if(!rst)
+				out <= 0;
+			else 
+				out <= out + 1;
+		end
 endmodule
