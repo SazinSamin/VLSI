b1f74e56da0c301958500a1794d9aabfe9153737
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-07-13 17:40:29 +0600

    Update deco_tb.v

--- Verilog/example/Decoder/deco_tb.v
+++ Verilog/example/Decoder/deco_tb.v
@@ -1,26 +1,18 @@
-module decode32_tb;
-	reg[4:0]a;
-	reg clk,rst;
-	wire[31:0]out;
-	integer i;
+module decoder_tb;
+wire [7:0] out;
+reg en;
+reg [2:0] in;
+integer i;
 
-	decode32 ut	(
-			.clk(clk),
-			.a(a),
-			.rst(rst),
-			.out(out)
-			);
+decoder dut(in,out,en);
 
-		initial begin
-				clk=0; a=0; rst=0;
-				#10 rst=1;
-				#2500 $finish;
-			end
-		initial 
-		begin
-		for(a=0;a<32;a=a+1)
-		#20; 
-		end
-		always #10 clk=~clk;
-		//always #20 a=a[i];
+initial begin  
+
+ $monitor( "en=%b, in=%d, out=%b ", en, in, out);
+   for ( i=0; i<16; i=i+1) 
+        begin
+           {en,in}  = i;
+            #1;
+        end
+end
 endmodule
