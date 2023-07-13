f37060e3bfd984086ceeca476d866183ca9c64ea
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-07-13 17:40:16 +0600

    Update deco.v

--- Verilog/example/Decoder/deco.v
+++ Verilog/example/Decoder/deco.v
@@ -1,22 +1,24 @@
-module decode32 (out,a,clk,rst);
-	input[4:0]a;
-	input clk,rst;
-	output reg[31:0]out;
-		reg[4:0]ai;
-	integer i;
-
-	always@(posedge clk)
-		begin
-			ai<=a;
-	if(~rst)
-		begin
-			out<=0;
+module decoder(in, out, en);
+	input [2:0] in;
+	input en;
+	output reg [7:0] out;
+	
+	always @ (in or en) begin
+		if(en) begin
+			out = 0;
+			case (in)
+				3'b000: out[0] = 1;
+				3'b001: out[1] = 1;
+              			3'b010: out[2] = 1;
+              			3'b011: out[3] = 1;
+              			3'b100: out[4] = 1;
+              			3'b101: out[5] = 1;
+              			3'b110: out[6] = 1;
+              			3'b111: out[7] = 1;
+			endcase
 		end
-	else
-		
-		for(i=0;i<32;i=i+1)
-			begin	
-				out[i]=(ai==i)?1'b1:1'b0;
-			end
-		end
+		else
+			out = 0;
+	end
+	
 endmodule
